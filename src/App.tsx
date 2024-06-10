980e43b47d85985a9353c7277723163151ae31de
Michelle More <michellemore28@gmail.com>
2024-06-10 11:31:58 -0500

    Add intertia to images

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,65 @@ function App() {
   const main = useRef<HTMLDivElement>(null);
   const { contextSafe } = useGSAP();
 
+  function getDirection(direction: string) {
+    const position = { x: "0", y: "0" };
+
+    switch (direction) {
+      case "left":
+        position.x = "-"; // Move left by a negative value
+        break;
+      case "right":
+        position.x = "+"; // Move right by a positive value
+        break;
+      case "up":
+        position.y = "-"; // Move up by a negative value
+        break;
+      case "down":
+        position.y = "+"; // Move down by a positive value
+        break;
+      case "left-up":
+        position.x = "-"; // Move left by a negative value
+        position.y = "-"; // Move up by a negative value
+        break;
+      case "left-down":
+        position.x = "-"; // Move left by a negative value
+        position.y = "+"; // Move down by a positive value
+        break;
+      case "right-up":
+        position.x = "+"; // Move right by a positive value
+        position.y = "-"; // Move up by a negative value
+        break;
+      case "right-down":
+        position.x = "+"; // Move right by a positive value
+        position.y = "+"; // Move down by a positive value
+        break;
+      default:
+        break;
+    }
+
+    return position;
+  }
+
   const drag = contextSafe(() => {
     Draggable.create(".hero_wrapper", {
       type: "x,y",
       bounds: window,
-      onDragStart: function () {
-        gsap.to(".hero_frame", {
-          duration: 0.2,
-          ease: "power1.out",
-        });
-      },
       onDragEnd: function () {
+        const direction = this.getDirection("start");
+
         gsap.to(".hero_frame", {
-          duration: 0.2,
+          duration: 1,
           ease: "power1.out",
+          x: `+=${
+            getDirection(direction).x != "0"
+              ? `${getDirection(direction).x}random([5,10,20])`
+              : 0
+          }`,
+          y: `+=${
+            getDirection(direction).y != "0"
+              ? `${getDirection(direction).y}random([5,10,20])`
+              : 0
+          }`,
         });
       },
     });
@@ -37,11 +82,11 @@ function App() {
 
   useGSAP(() => {
     gsap.from(".hero_frame", {
-      duration: 2,
+      duration: 1.8,
       scale: 2,
       ease: "power1.out",
       onComplete: drag,
-      stagger: 0.08,
+      stagger: 0.2,
       opacity: 0,
     });
   }, []);
